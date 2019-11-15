788fd734107d3eb5a344d8195b8e0f2a0d8d7f7c
Ken <vuken30227@gmail.com>
2019-11-15 21:17:48 +0000

    Used DataFrame.head and DataFrame.describe to show the data

--- Intro_to_pandas/main.py
+++ Intro_to_pandas/main.py
@@ -12,4 +12,12 @@ data = pd.Series([2300, 23000])
 
 # To create a DataFrame, pass a dict mapping.
 dataFrame = pd.DataFrame({'sample':sample, 'data':data})
-print(dataFrame)
+#print(dataFrame)
+# If the two series don't match in length, the missing values are filled with NA/NaN values
+
+# To load an entire file into a DataFrame:
+california_housing_df = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv", sep=",")
+#print(california_housing_df.describe())
+
+# Prints the first few records of DataFrame
+print(california_housing_df.head)
